1ba7dfef7032cedac6c85bf1bd222f9f8048d035
lcbiplove <lcbiplove1@gmail.com>
2020-06-24 09:54:40 +0545

    Theme cookie

--- play-snake/js/snake-max.js
+++ play-snake/js/snake-max.js
@@ -1108,25 +1108,56 @@ SNAKE.Board = SNAKE.Board || (function() {
         if (config.boardContainer) {
             me.setBoardContainer(config.boardContainer);
         }
+        if(getCookie("ck__th")==="null"){
+            changeTheme('css/dark-theme.css?' + Math.random());
+            document.querySelector("#select option:last-child").setAttribute("selected", "");
+            document.querySelector("#select option:first-child").removeAttribute("selected");
+        }
         document.getElementById("select").onchange = function(){
-            function changeTheme (Theme) {
-                document.getElementById('style').setAttribute('href', Theme);
-            }
+            console.log();
             var index = document.getElementById("select").selectedIndex;
             switch (index) {
                 case 0: 
                   changeTheme('css/light-theme.css?' + Math.random());
+                  eraseCookie("ck__th");
                   break;
                 case 1: 
                   changeTheme('css/dark-theme.css?' + Math.random());
+                  setCookie("ck__th", "null", 365);
                   break;            
                 default:
                   changeTheme('css/light-theme.css?' + Math.random());
+                  eraseCookie("ck__th");
                   break;
             }
             setTimeout(function() {
                 document.getElementById('game-area').focus();
             }, 10);
         };
+        function changeTheme (Theme) {
+            document.getElementById('style').setAttribute('href', Theme);
+        }
+        function setCookie(name,value,days) {
+            var expires = "";
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days*24*60*60*1000));
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = name + "=" + (value || "")  + expires + ";";
+        }
+        function getCookie(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        }
+        function eraseCookie(name) {   
+            document.cookie = name+'=; Max-Age=-99999999;';  
+        }
     }; // end return function
 })();  
